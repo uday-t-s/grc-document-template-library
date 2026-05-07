ade80fa66ab60300335203052f493085da9e0400
Uday T S <your-udayts2000@gmail.com>
2026-05-07 23:15:04 +0530

    Day 6 — Added routing and protected pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import AuthPage from "./pages/AuthPage";
+import TemplateList from "./pages/TemplateList";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return <AuthPage />;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthPage
+              setIsAuthenticated={setIsAuthenticated}
+            />
+          }
+        />
+
+        <Route
+          path="/templates"
+          element={
+            <ProtectedRoute
+              isAuthenticated={isAuthenticated}
+            >
+              <TemplateList />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
